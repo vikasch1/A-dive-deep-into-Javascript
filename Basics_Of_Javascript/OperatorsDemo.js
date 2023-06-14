3c55db77a2bfe31b753cb79dc87e7a2a48f46605
Vikas Chaudhary <vikassixfeet@gmail.com>
2023-06-14 13:23:38 +0530

    NOTES UPDATED TILL BITIWISE OPERATORS

--- Basics_Of_Javascript/OperatorsDemo.js
+++ Basics_Of_Javascript/OperatorsDemo.js
@@ -1,30 +1,89 @@
-//arithmatic operators
+// //arithmatic operators
+
+// let x =10;
+// let y=3;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(y**2); //exponent
+
+
+// //assignment ops
+
+// let a =10;
+// a+=10;
+// console.log("value of a after addition =",a);
+// a-=3;
+// console.log("value of a after subtraction =",a);
+// a*=5;
+// console.log("value of a after multiplication =",a);
+// a/=2;
+// console.log("value of a after division =",a);
+// a%=3;
+// console.log("value of a after modulo/remiander =",a);
+// a**=2;
+// console.log("value of a after exponent  =",a);
+
+//Relational operators
 
 let x =10;
-let y=3;
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x/y);
-console.log(x%y);
-console.log(y**2); //exponent
+let y=20;
+let z =5;
+let a=10;
+console.log("x < z =",x<z);
+console.log("y > z =",y>z);
+console.log("x <= z =",x<=z);
+console.log("x >= a =",x>=a);
 
 
-//assignment ops
+//logical
+console.log(true && false); 
+console.log((10>5)&& (6<3));
+console.log(false || false);
+console.log((5<10)||(3>8));
+console.log(!(3>6));
+console.log(!(true && true));
 
-let a =10;
-a+=10;
-console.log("value of a after addition =",a);
-a-=3;
-console.log("value of a after subtraction =",a);
-a*=5;
-console.log("value of a after multiplication =",a);
-a/=2;
-console.log("value of a after division =",a);
-a%=3;
-console.log("value of a after modulo/remiander =",a);
-a**=2;
-console.log("value of a after exponent  =",a);
+
+console.log(10 && 6); // output is 6
+// 10 and 6 are truthy value in js
+// 10 is a truth value so and gate directly return second value
+
+console.log(6 && 10);
+
+
+console.log(10 || 0); //10
+console.log(6||10); //6
+console.log(0 ||-0); //-0
+
+
+console.log(10/null);
+console.log(undefined/null);
+
+console.log(null==null);
+console.log(null===null);
+
+console.log(NaN==NaN);
+console.log(NaN===NaN);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Basics_Of_Javascript/specialnumbers.js
@@ -0,0 +1,13 @@
+console.log(0);
+console.log(-0);
+console.log(NaN);
+console.log(Infinity);
+console.log(-Infinity);
+
+console.log(10<Infinity); //true
+console.log(10>NaN); //false
+console.log(null==NaN); //false
+
+
+
+
