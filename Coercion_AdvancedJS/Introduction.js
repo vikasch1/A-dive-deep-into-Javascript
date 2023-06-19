c23d9bb719af52ff718cd3ccbdd4f69656780b1f
Vikas Chaudhary <vikassixfeet@gmail.com>
2023-06-19 21:45:46 +0530

    Introduction to Coercion started

---
+++ Coercion_AdvancedJS/Introduction.js
@@ -0,0 +1,40 @@
+console.log(1+1); //2
+console.log("1"+1); //11
+console.log("1"-1); //0
+
+console.log(null-1); //null converts into +0.result =-1
+console.log(undefined-1); //undefined converts into NaN,NaN-1=NaN
+
+console.log(10-1); //9
+console.log(10-null); //10-0=10
+console.log(10-undefined); //10-Nan-nan {10-invalid number =invalid number}
+
+console.log(true-2); //1-2=-1
+
+console.log(2-null); //2-0=2
+console.log(2-undefined);//2-nan=nan
+
+console.log("12"*2);//24
+console.log(1-"453");//-452
+
+console.log(1-"hfyfyr"); //1- invalid =invalid - nan
+console.log(1-"0xa"); //1- valid(some number system)=valid output=-9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
