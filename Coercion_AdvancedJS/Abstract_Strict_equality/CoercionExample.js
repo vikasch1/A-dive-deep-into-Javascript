465fabfee0d7432f0f5df64c53a99cada27f7c06
Vikas Chaudhary <vikassixfeet@gmail.com>
2023-06-23 21:00:53 +0530

    few more corner cases on coercion

---
+++ Coercion_AdvancedJS/Abstract_Strict_equality/CoercionExample.js
@@ -0,0 +1,20 @@
+let obj ={
+    x:10,
+    y:20
+};
+let num=45;
+
+console.log("my roll no. is "+34);
+console.log("my roll no. is ",34);
+console.log(`my object is ${obj}`); //[obj,obj]- converting obj to string thats why it shows obj,obj
+console.log( `my number is ${num}`);
+
+console.log(1<2<3); //true-1<2-true<3-1<3-true
+console.log(3>2>1); //false-3>2-true>1-1>1-false
+
+
+
+
+
+
+
